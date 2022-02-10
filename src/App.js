7d3b9582c35acb03ab073e0a205bc2ea892647f4
Tiffany <yang.ding@hotmail.com>
2022-02-10 13:04:09 -0800

    removed original code for restructuring

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import GlobalStyle from './GlobalStyle';
-
-// React router
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-
-// import main components
-import Home from './pages/Home';
-import Stories from './pages/Stories';
-import Features from './pages/Features';
-import Pricing from './pages/Pricing';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
-      <GlobalStyle />
-
-      {/* ScrollToTop component allows page to scroll to top on page change in mobile view */}
-      <ScrollToTop /> 
+    <div>
       
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/stories'>
-          <Stories />
-        </Route>
-        <Route exact path='/features'>
-          <Features />
-        </Route>
-        <Route exact path='/pricing'>
-          <Pricing />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default App
