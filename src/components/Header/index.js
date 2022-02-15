fcb8fb8e8802e971f9ba6e57ee5ada960d34b680
Tiffany <yang.ding@hotmail.com>
2022-02-15 10:19:41 -0800

    added mobile menu toggle functionality

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 import { StyledHeader } from './Header.styled';
 import MainLogo from '../../assets/shared/desktop/logo.svg';
-import Nav from '../Nav'
+import Nav from '../Nav';
 import { ReactComponent as HamburgerIcon } from '../../assets/shared/mobile/menu.svg';
 import { ReactComponent as ExitIcon } from '../../assets/shared/mobile/close.svg';
 
-
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <StyledHeader>
       <div className='mobile-header-container'>
         <img src={MainLogo} alt='Photosnap logo' />
-        <HamburgerIcon />
-        <div className='nav-modal'>
-          <Nav />
-        </div>
+        <HamburgerIcon
+          onClick={(e) => {
+            setShowMenu(!showMenu);
+          }}
+        />
+        {showMenu && (
+          <div className='nav-modal'>
+            <Nav />
+          </div>
+        )}
       </div>
       <div className='header-container'></div>
     </StyledHeader>
