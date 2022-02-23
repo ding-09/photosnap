0ef4c383144c1221e6343b79a42d78878be5b976
Tiffany <yang.ding@hotmail.com>
2022-02-23 15:52:01 -0800

    added Beta component to Pricing page

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,20 +1,28 @@
 import Hero from '../Hero';
+import Beta from '../Beta';
 import PricingContent from './PricingContent';
+import { StyledPricing } from './Pricing.styled';
 import { useEffect } from 'react';
 
 const Pricing = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
-    <>
+    <StyledPricing>
       <Hero
         img={PricingContent.heroImg}
         heroHeading={PricingContent.heroHeading}
         heroDesc={PricingContent.heroDesc}
         hasBorder={true}
       />
-    </>
+      <section className="price-card-group">
+
+      </section>
+      <section className="price-table"></section>
+      <Beta />
+    </StyledPricing>
   );
 };
 
