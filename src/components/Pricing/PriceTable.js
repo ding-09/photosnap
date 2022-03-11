3ce7581e28e6701c21072aeb3d460e66acafc5a4
Tiffany <yang.ding@hotmail.com>
2022-03-11 11:11:46 -0800

    added unique id to Pricing components

--- src/components/Pricing/PriceTable.js
+++ src/components/Pricing/PriceTable.js
@@ -1,5 +1,6 @@
 import PriceTableRow from './PriceTableRow';
 import { StyledPriceTable } from './PriceTable.styled';
+import { v4 as uuidv4 } from 'uuid';
 
 const PriceTable = ({ features }) => {
   return (
@@ -7,7 +8,7 @@ const PriceTable = ({ features }) => {
       <caption>The Features</caption>
       <tbody>
         {features.map((feature) => (
-          <PriceTableRow feature={feature} />
+          <PriceTableRow feature={feature} key={uuidv4()} />
         ))}
       </tbody>
     </StyledPriceTable>
