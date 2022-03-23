cd7893ddd553a309cb189244aa714f8d179b8236
Tiffany <yang.ding@hotmail.com>
2022-03-23 15:32:48 -0700

    added tablet responsive styles for Beta component

--- src/components/Beta.js
+++ src/components/Beta.js
@@ -1,10 +1,9 @@
 import { StyledBeta } from './Beta.styled';
 import { ArrowButton } from '../styles/Buttons';
-import betaBg from '../assets/shared/mobile/bg-beta.jpg'
 
 const Beta = () => {
   return (
-    <StyledBeta bgImg={betaBg}>
+    <StyledBeta>
       <h2>We're in beta. Get your invite today!</h2>
       <ArrowButton innerText='Get an invite' color='#fff' />
       <div className="hero-border"></div>
--- src/components/Beta.styled.js
+++ src/components/Beta.styled.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
+// BG IMGS
+import bgBeta from '../assets/shared/desktop/bg-beta.jpg';
+
 export const StyledBeta = styled.section`
-  background-image: url(${(props) => props.bgImg});
+  background-image: url(${bgBeta});
   background-size: cover;
   background-repeat: no-repeat;
   min-height: 28rem;
@@ -22,6 +25,29 @@ export const StyledBeta = styled.section`
     justify-content: flex-start;
     svg {
       margin-left: 2rem;
+    }
+  }
+
+  /* MEDIA QUERIES */
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+    h2 {
+      font-size: 4rem;
+      letter-spacing: 0.4rem;
+      line-height: 4.8rem;
+      flex: 60%;
+    }
+    button {
+      flex: 40%;
+      justify-content: flex-end;
+      margin-right: 1rem;
+      svg {
+        margin-left: 0;
+      }
+    }
+    .hero-border {
+      height: 100%;
     }
   }
 `;
