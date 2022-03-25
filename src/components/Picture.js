f8debadd288ff939eb229547226a617e88a76a8a
Tiffany <yang.ding@hotmail.com>
2022-03-25 15:26:57 -0700

    fixed image order issue with Picture component

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Picture = ({ imgs }) => {
   return (
     <picture>
+      <source media='(min-width: 769px)' srcSet={imgs.desktop} />
       <source media='(min-width: 768px)' srcSet={imgs.tablet} />
-      <source media='(min-width: 1024px)' srcSet={imgs.desktop} />
       <img src={imgs.mobile} alt='' />
     </picture>
   );
